00064752787933b908b8fe439290298ee2e07872
PradipkumarMane <134221047+PradipkumarMane@users.noreply.github.com>
2024-09-19 20:02:58 +0530

    Update Lab_Exam_Student_Q3.cpp

--- Lab_Exam_Student_Q3.cpp
+++ Lab_Exam_Student_Q3.cpp
@@ -1,3 +1,8 @@
+// Create a class 'Student' with three data members which are name, age and address. The constructor of the class assigns default values to name as "unknown", age as '0' and address as "not available". It has two functions with the same name 'setInfo'. First function has two parameters for name and age and assigns the same whereas the second function takes has three parameters which are assigned to name, age and address respectively. Print the name, age and address of 10 students.
+//  Use array of objects
+
+// Name:Pradipkumar Mane
+// ROLL_NO:36
 #include<iostream>
 using namespace std;
 class Student
